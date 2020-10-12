a650bd3f046d1c3bd31d12131b98d2aa2bb5c180
AndyGeek <andy.eulogio@gmail.com>
2020-10-12 12:15:55 -0500

    Fix errors

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../button/Button";
 
@@ -9,7 +9,7 @@ export const Navbar = () => {
    const [button, setButton] = useState(true);
 
    const handleClick = () => setClick(!click);
-   const closeMobileMenu = () => setClick(true);
+   const closeMobileMenu = () => setClick(false);
 
    const showButton = () => {
       if (window.innerWidth <= 960) {
@@ -19,13 +19,19 @@ export const Navbar = () => {
       }
    };
 
+   // ----------------
+   useEffect(() => {
+      console.log("XX");
+      showButton();
+   }, []);
+
    window.addEventListener("resize", showButton);
 
    return (
       <>
          <nav className="navbar">
             <div className="navbar-container">
-               <Link to="/" className="navbar-logo">
+               <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                   TRVL <i className="fab fa-typo3" />
                </Link>
                <div className="menu-icon" onClick={handleClick}>
